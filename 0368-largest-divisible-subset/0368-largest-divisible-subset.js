62b45a4f1fc284a34bd356749e6f732520ca0927
Nilesh Prajapati <99721281+Nilesh2697@users.noreply.github.com>
2024-02-09 20:37:38 +0530

    Time: 79 ms (53.16%), Space: 52.1 MB (17.72%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.js
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.js
@@ -3,16 +3,14 @@
  * @return {number[]}
  */
 var largestDivisibleSubset = function(nums) {
-    nums = nums.sort((x, y) => x - y);
-    let p = [], m = nums.length - 1;
-    for (let i = nums.length - 1; i >= 0; i--) {
-        p[i] = [1, undefined];
-        for (let j = i + 1; j < nums.length; j++)
-            if (nums[j] % nums[i] === 0 && p[i][0] <= p[j][0]) p[i] = [p[j][0] + 1, j];
-        if (p[m][0] < p[i][0]) m = i;
+nums.sort((a,b)=>a-b)
+    var dp = Array.from({length: nums.length}, (v,k) => [nums[k]]);
+    for (let i=nums.length-1; i>=0; i--) {
+        for (let j = i+1; j<nums.length; j++) {
+            if (dp[i].length<=dp[j].length && dp[j][0]%nums[i]===0) {
+                dp[i] = [nums[i], ...dp[j]];
+            }
+        }
     }
-
-    let ans = [];
-    for (let i = m; i !== undefined; i = p[i][1]) ans.push(nums[i]);
-    return ans;
+    return dp.reduce((a,x) => a.length > x.length ? a : x);
 };
