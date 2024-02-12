2e4f8d76f5ed76f73a6b2b96cad0ab2baa14db32
Nilesh Prajapati <99721281+Nilesh2697@users.noreply.github.com>
2024-02-12 19:03:02 +0530

    Time: 66 ms (32.07%), Space: 53.2 MB (6.84%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+     let count = 0;
+    let candidate = null;
+    
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    
+    count = 0;
+    for (let num of nums) {
+        if (num === candidate) {
+            count++;
+        }
+    }
+    
+    return count > nums.length / 2 ? candidate : null;
+};
